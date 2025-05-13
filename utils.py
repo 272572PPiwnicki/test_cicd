d915bf7bb64229f28c88296ba257cba7cc404eb1
Patryk Piwnicki <272572@student.pwr.edu.pl>
2025-05-13 23:32:06 +0200

    fixe coderate

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
+"""Utility functions for basic arithmetic operations."""
+
 def add(a: int, b: int) -> int:
+    """Return the sum of a and b."""
     return a + b
 
 def subtract(a: int, b: int) -> int:
+    """Return the difference when b is subtracted from a."""
     return a - b
 
 def multiply(a: int, b: int) -> int:
+    """Return the product of a and b."""
     return a * b
 
 def divide(a: int, b: int) -> float:
+    """Return the quotient when a is divided by b."""
     return a / b
